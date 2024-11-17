212133e953d33c10a664b05c403ea0e749b0abcf
nedyalkopmihalev <nedialko81@abv.bg>
2024-11-17 19:15:45 +0200

    Run Vue in Laravel

--- Dockerfile
+++ Dockerfile
@@ -20,7 +20,7 @@ WORKDIR /var/www
 COPY . .
 
 RUN composer install
-RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+RUN curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 RUN apt-get install -y nodejs
 RUN npm install && npm run build
 
--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,14 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Listen on all interfaces
+        port: 5173,      // Use the same port
+        strictPort: true, // Ensure the port does not change
+        hmr: {
+            host: 'localhost', // Replace with your Docker host's IP if needed
+        },
+    },
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
